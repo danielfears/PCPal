7266a76c8ab81e2614c17565c0494244fc2b8979
Daniel Fears <danieljfears@gmail.com>
2015-03-15 11:39:43 +0000

    Popup layouts created and linked to each component - names changes
    components layout almost complete - separate popup layouts need
    populating

--- app/src/main/java/danieljfears/devproject/ComponentGuide.java
+++ app/src/main/java/danieljfears/devproject/ComponentGuide.java
@@ -15,7 +15,9 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import java.text.ParsePosition;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,18 +36,18 @@ public class ComponentGuide extends ActionBarActivity {
 
         components = new ArrayList<>();
 
-        components.add("drawable/cddrive");
-        components.add("drawable/cooler");
-        components.add("drawable/cpu");
-        components.add("drawable/fans");
-        components.add("drawable/gpu");
-        components.add("drawable/harddrive");
-        components.add("drawable/memory");
-        components.add("drawable/motherboard");
-        components.add("drawable/operatingsystem");
         components.add("drawable/pccase");
+        components.add("drawable/motherboard");
+        components.add("drawable/cpu");
+        components.add("drawable/cooler");
+        components.add("drawable/memory");
         components.add("drawable/powersupply");
+        components.add("drawable/harddrive");
+        components.add("drawable/gpu");
+        components.add("drawable/cddrive");
+        components.add("drawable/operatingsystem");
         components.add("drawable/satacable");
+        components.add("drawable/fans");
 
         ComponentAdapter adapter = new ComponentAdapter(ComponentGuide.this);
         gridView.setAdapter(adapter);
@@ -54,32 +56,48 @@ public class ComponentGuide extends ActionBarActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                showPopup(ComponentGuide.this,null);
+                if(components.get(position) == "drawable/pccase"){
+                    showCasePopup(ComponentGuide.this,null);
+                }
+                else if(components.get(position) == "drawable/motherboard"){
+                    showMotherboardPopup(ComponentGuide.this,null);
+                }
+                else if(components.get(position) == "drawable/cpu"){
+                    showCpuPopup(ComponentGuide.this,null);
+                }
+                else if(components.get(position) == "drawable/cooler"){
+                    showCoolerPopup(ComponentGuide.this,null);
+                }
+                else if(components.get(position) == "drawable/memory"){
+                    showMemoryPopup(ComponentGuide.this,null);
+                }
+                else if(components.get(position) == "drawable/powersupply"){
+                    showPowersupplyPopup(ComponentGuide.this,null);
+                }
+                else if(components.get(position) == "drawable/harddrive"){
+                    showHarddrivePopup(ComponentGuide.this,null);
+                }
+                else if(components.get(position) == "drawable/gpu"){
+                    showGpuPopup(ComponentGuide.this,null);
+                }
+                else if(components.get(position) == "drawable/cddrive"){
+                    showCddrivePopup(ComponentGuide.this,null);
+                }
+                else if(components.get(position) == "drawable/operatingsystem"){
+                    showOperatingsystemPopup(ComponentGuide.this,null);
+                }
+                else if(components.get(position) == "drawable/satacable"){
+                    showSatacablePopup(ComponentGuide.this,null);
+                }
+                else if(components.get(position) == "drawable/fans"){
+                    showFansPopup(ComponentGuide.this,null);
+                }
+
+                Toast.makeText(ComponentGuide.this, components.get(position), Toast.LENGTH_SHORT).show();
 
             }
         });
-    }
 
-    // The method that displays the popup.
-    private void showPopup(final Activity context, Point p) {
-        int popupWidth = 600;
-        int popupHeight = 850;
-
-        // Inflate the popup_layout.xml
-        RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.popup);
-        LayoutInflater layoutInflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View layout = layoutInflater.inflate(R.layout.popup_layout, viewGroup);
-
-        // Creating the PopupWindow
-        final PopupWindow popup = new PopupWindow(context);
-        popup.setContentView(layout);
-        popup.setWidth(popupWidth);
-        popup.setHeight(popupHeight);
-        popup.setFocusable(true);
-
-        // Displaying the popup at the specified location, + offsets.
-        popup.showAtLocation(layout, Gravity.CENTER, 0, 100);
     }
 
     private class ComponentAdapter extends BaseAdapter {
@@ -123,6 +141,282 @@ public class ComponentGuide extends ActionBarActivity {
 
 
     }//end top image adapter
+
+    // The method that displays the popup.
+    private void showCasePopup(final Activity context, Point p) {
+        int popupWidth = 600;
+        int popupHeight = 850;
+
+        // Inflate the popup_layout.xml
+        RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popup_case, viewGroup);
+
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 100);
+    }
+
+    // The method that displays the popup.
+    private void showMotherboardPopup(final Activity context, Point p) {
+        int popupWidth = 600;
+        int popupHeight = 850;
+
+        // Inflate the popup_layout.xml
+        RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popup_motherboard, viewGroup);
+
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 100);
+    }
+
+    // The method that displays the popup.
+    private void showCpuPopup(final Activity context, Point p) {
+        int popupWidth = 600;
+        int popupHeight = 850;
+
+        // Inflate the popup_layout.xml
+        RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popup_cpu, viewGroup);
+
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 100);
+    }
+
+    // The method that displays the popup.
+    private void showCoolerPopup(final Activity context, Point p) {
+        int popupWidth = 600;
+        int popupHeight = 850;
+
+        // Inflate the popup_layout.xml
+        RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popup_cooler, viewGroup);
+
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 100);
+    }
+
+    // The method that displays the popup.
+    private void showMemoryPopup(final Activity context, Point p) {
+        int popupWidth = 600;
+        int popupHeight = 850;
+
+        // Inflate the popup_layout.xml
+        RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popup_memory, viewGroup);
+
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 100);
+    }
+
+    // The method that displays the popup.
+    private void showPowersupplyPopup(final Activity context, Point p) {
+        int popupWidth = 600;
+        int popupHeight = 850;
+
+        // Inflate the popup_layout.xml
+        RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popup_psu, viewGroup);
+
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 100);
+    }
+
+    // The method that displays the popup.
+    private void showHarddrivePopup(final Activity context, Point p) {
+        int popupWidth = 600;
+        int popupHeight = 850;
+
+        // Inflate the popup_layout.xml
+        RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popup_harddrive, viewGroup);
+
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 100);
+    }
+
+    // The method that displays the popup.
+    private void showGpuPopup(final Activity context, Point p) {
+        int popupWidth = 600;
+        int popupHeight = 850;
+
+        // Inflate the popup_layout.xml
+        RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popup_gpu, viewGroup);
+
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 100);
+    }
+
+    // The method that displays the popup.
+    private void showCddrivePopup(final Activity context, Point p) {
+        int popupWidth = 600;
+        int popupHeight = 850;
+
+        // Inflate the popup_layout.xml
+        RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popup_cddrive, viewGroup);
+
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 100);
+    }
+
+    // The method that displays the popup.
+    private void showOperatingsystemPopup(final Activity context, Point p) {
+        int popupWidth = 600;
+        int popupHeight = 850;
+
+        // Inflate the popup_layout.xml
+        RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popup_os, viewGroup);
+
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 100);
+    }
+
+    // The method that displays the popup.
+    private void showSatacablePopup(final Activity context, Point p) {
+        int popupWidth = 600;
+        int popupHeight = 850;
+
+        // Inflate the popup_layout.xml
+        RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popup_sata, viewGroup);
+
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 100);
+    }
+
+    // The method that displays the popup.
+    private void showFansPopup(final Activity context, Point p) {
+        int popupWidth = 600;
+        int popupHeight = 850;
+
+        // Inflate the popup_layout.xml
+        RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popup_fans, viewGroup);
+
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 100);
+    }
 }
 
 
