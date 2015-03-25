4149ce729c1a57aed833fa0d725abfe22cc11f33
Daniel Fears <danieljfears@gmail.com>
2015-03-25 19:31:40 +0000

    Images changed to open sourced

--- app/src/main/java/danieljfears/devproject/ComponentGuide.java
+++ app/src/main/java/danieljfears/devproject/ComponentGuide.java
@@ -7,8 +7,6 @@ import android.graphics.Point;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +17,6 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
